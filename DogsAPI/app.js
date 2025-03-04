81f6601d59c140fb524dcf85a4f392923d672619
Ganesh <153377877+Gen-Loper@users.noreply.github.com>
2025-03-04 22:03:37 +0530

    Update app.js
    Comments are added

--- DogsAPI/app.js
+++ DogsAPI/app.js
@@ -1,6 +1,8 @@
+// Accessing the button and div to generate and show the result
 let btn = document.querySelector("button");
 let resultImage = document.querySelector("#result");
 
+// Event Listener that handles result when button is pressed
 btn.addEventListener("click",async()=> {
     console.log("button Clicked");
     let img = await getImage();
@@ -12,8 +14,9 @@ btn.addEventListener("click",async()=> {
     breeds.innerHTML= src;
 });
 
-
+// url for the DogsAPI
 let url = "https://dog.ceo/api/breeds/image/random";
+//Requesting to the API
 async function getImage(params) {
     try {
         let res= await axios.get(url);
